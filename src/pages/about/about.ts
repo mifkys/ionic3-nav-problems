6e564bbabbe45638982cde850c837625bf546bb2
Admin <admin@Mac-Admin.local>
2017-08-25 14:09:53 +0300

    Initial commit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Events, IonicPage } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public events: Events) {}
 
+  catchEvent() {
+    this.events.publish('event:catched');
+  }
+
+  ionViewDidLoad() {
+    console.log('ioniViewDidLoad AboutPage');
+    this.events.subscribe('event:catched', () => {
+      console.log('event catched');
+    });
+  }
+
+  ionViewWillLeave() {
+    console.log('ioniViewWillLeave AboutPage');
+  }
+
+  ionViewDidLeave() {
+    console.log('ioniViewDidLeave AboutPage');
+  } 
+
+  ionViewWillUnload() {
+    console.log('ioniViewWillUnload AboutPage');
+    this.events.unsubscribe('event:catched');
   }
 
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor() {
 
   }
 
